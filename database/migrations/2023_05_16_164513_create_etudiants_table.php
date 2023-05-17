6c2f8c682d8d13c345b78aafa56734fa39a4fd2f
Imad Asserne <imad.asserne@gmail.com>
2023-05-17 20:43:23 +0000

    create table

---
+++ database/migrations/2023_05_16_164513_create_etudiants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('sexe');
+            $table->unsignedBigInteger('filiere_id');
+            $table->timestamps();
+            $table->foreign('filiere_id')->references('id')->on('filieres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
