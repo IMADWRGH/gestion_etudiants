2845f4816fecd835a80e2672802a36bcb432a464
Imad Asserne <imad.asserne@gmail.com>
2023-05-20 01:20:24 +0000

    add Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FiliereController;
-use App\Http\Controllers\EtudiantController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,5 +17,15 @@ use App\Http\Controllers\EtudiantController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('etudiants', EtudiantController::class);
-Route::resource('filiere', FiliereController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
